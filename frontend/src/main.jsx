c1eda7fab6345d55c9525979a0ce0a162d4cc9f0
Ravi Kumar <ravibseb96@gmail.com>
2025-03-15 22:24:59 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { UserContextProvider } from "./context/userContext.jsx";
 import { PostContextProvider } from "./context/PostContext.jsx";
 import { ChatContextProvider } from "./context/ChatContext.jsx";
 import { SocketContextProvider } from "./context/SocketContext.jsx";
+import { UserContextProvider } from "./context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
